bfc7d65d28177c6fc4f0ee272249b612b3d91a0c
OwenStewart5 <owenstewart2005@gmail.com>
2025-10-16 18:40:01 -0400

    icon larger

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -233,7 +233,7 @@ export default function Header({ onMobileMenuChange }: HeaderProps) {
                   variant="ghost"
                   className="md:hidden p-4 text-yellow-400 hover:bg-yellow-100 [text-shadow:_1px_1px_1px_rgb(0_0_0_/_100%)]"
                 >
-                  <Menu className="h-12 w-12" />
+                  <Menu className="h-16 w-16" />
                 </Button>
               </SheetTrigger>
               <SheetPortal>
